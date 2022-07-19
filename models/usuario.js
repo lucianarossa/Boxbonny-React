91cc543a406605170b0546eca871d72b23ea1020
[mayra <[mayradelvallegonzalez@gmail.com]>
2022-07-19 16:49:19 -0300

    googleCredentials

--- models/usuario.js
+++ models/usuario.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const usuarioSchema = new mongoose.Schema({
-    nombre:{type: String, required:true},
-    apellido:{type: String, required:true},
-    email:{type: String, required:true},
-    contraseña:{type: String, required:true},
-    imagen:{type: String, required:true},
-    uniqueString: {type: String, required:true},
-    verification: {type: Boolean, required:true}
+    nombre: { type: String, required: true },
+    apellido: { type: String, required: true },
+    email: { type: String, required: true },
+    contraseña: { type: String, required: true },
+    imagen: { type: String, required: true },
+    uniqueString: { type: String, required: true },
+    verification: { type: Boolean, required: true }
 })
 
 const Usuario = mongoose.model('usuarios', usuarioSchema)
