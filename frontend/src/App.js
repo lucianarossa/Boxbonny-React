54ca8cd6fed00a4f41f3c1b8f690b5ab28ae2da1
kevocode22 <ocampokevin14@gmail.com>
2022-07-19 09:44:08 -0300

    cambios en App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './styles/App.css';
+import PacksComponent from './components/PacksComponent'
 
 
 function App() {
   return (
     <div className="App">
+       <PacksComponent/>
     </div>
   );
 }
