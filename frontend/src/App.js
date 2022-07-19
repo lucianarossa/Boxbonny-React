2d268b32b0af4a0a5290a7ce3613d26848007b17
alexishermosa <alexishermosa@hotmail.com>
2022-07-19 11:06:15 -0300

    esqueleto base del nav y footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
+
 import './styles/App.css';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="app flex-column" >
+      <Nav />
+      <Footer />
     </div>
   );
 }
