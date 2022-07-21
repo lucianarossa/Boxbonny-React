becb12f53a9c18942b96ad2cd99f6a2e8818483a
alexishermosa <alexishermosa@hotmail.com>
2022-07-21 17:57:22 -0300

    ruta carrito

---
+++ frontend/src/helpers/RenderTop.js
@@ -0,0 +1,7 @@
+import { useEffect } from "react"; 
+import { useLocation } from "react-router-dom"; 
+export default function RenderTop() { 
+    const { pathname } = useLocation(); 
+    useEffect(() => { 
+        window.scrollTo(0, 0); 
+    }, [pathname]); return null; }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={reduxStore}>
     <BrowserRouter>
-      <ScrollToTop />
+      {/* <ScrollToTop /> */}
       <App />
     </BrowserRouter>
   </Provider>
