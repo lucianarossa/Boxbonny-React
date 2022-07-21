a91fb5314546ab452e8543cab9825724c03a924c
kevocode22 <ocampokevin14@gmail.com>
2022-07-21 18:26:06 -0300

    Registro terminado

--- frontend/src/components/SingnIn.jsx
+++ frontend/src/components/SingnIn.jsx
@@ -68,8 +68,8 @@ const SignIn = () => {
             })
         }  
         if (res?.data.success) {
-            console.log("Te entrooo")
-         container.classList.remove("sign_up_mode") 
+            const container1 = document.querySelector(".containerRegistro");
+            container1.classList.remove("sign_up_mode") 
         toast.success(res.data.message)
         } else {
             toast.error(res.data.message)
@@ -100,7 +100,6 @@ const SignIn = () => {
 
                         <div className="social_media">
                             <GoogleSignIn />
-
                         </div>
 
                     </form>
