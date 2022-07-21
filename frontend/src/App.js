076c394fcad4a0a5e297b81b8fce184e71f216e7
luciana rossa <lucianarossa@gmail.com>
2022-07-21 17:29:08 -0300

    proteccion de rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,13 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import packsActions from "./redux/actions/packsActions"
 import PacksPage from './pages/PacksPage';
 import Index from './pages/Index';
 import Upload from "./components/Upload"
 import OneExperiencePage from './pages/OneExperiencePage';
 import SignIn from './components/SingnIn'
-import SignUp from './components/SignUp'
 import ShoppingCart from './components/ShoppingCart';
 import PackDetails from './components/PackDetails';
 
@@ -24,18 +23,20 @@ function App() {
     // eslint-disable-next-line
   }, [])
 
+  const user = useSelector(store => store.usuariosReducer.user) 
+
 
   return (
     <div className="app ">
       <Nav />
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="*" element={<Index />} />
         <Route path="/packs" element={<PacksPage />} />
         <Route path="/administrador" element={<Upload />} />
         <Route path="/packs/packdetails/:id" element={<PackDetails />} />
         <Route path="/packs/oneexperience/:id" element={<OneExperiencePage />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        {!user && <Route path="/signin" element={<SignIn />} />}
         <Route path="/cart" element={<ShoppingCart />} />
       </Routes>
       <Footer />
