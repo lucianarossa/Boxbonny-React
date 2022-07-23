335f7cb7e52d062fb002df94eafbb8507d0a57d1
VaRaOr <valen15k@gmail.com>
2022-07-23 14:37:12 -0300

    server3

--- controllers/experienciasControllers.js
+++ controllers/experienciasControllers.js
@@ -57,7 +57,7 @@ const experienciasControllers = {
 				console.log("ACA LLEGO");
                 const filename = crypto.randomBytes(10).toString('hex') + "." + files.imagen.name.split(".")[files.imagen.name.split(".").length - 1]
                 console.log("DIRNAME", __dirname);
-				const ruta = `${__dirname}/../client/build/media/${filename}`
+				const ruta = `${__dirname}/../storage/experiencias/${filename}`
 				console.log("RUTAAAAA",ruta);
                 files.imagen.mv(ruta, err => {
                     if (err) {
@@ -72,7 +72,7 @@ const experienciasControllers = {
                     incluye: incluye,
                     direccion: direccion,
                     ciudad: ciudad,
-                    imagen: "https://boxbonny-back.herokuapp.com/client/build/media/" + filename,
+                    imagen: "https://boxbonny-back.herokuapp.com/storage/experiencias/" + filename,
                     pack: pack
                 }).save()
                 nuevaExperiencia = await Pack.findOneAndUpdate({ _id: pack }, { $push: { experiencias: experiencia._id } }, { new: true })
--- server.js
+++ server.js
@@ -18,14 +18,13 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use('/api', Router);
 
-if(process.env.NODE_ENV === 'production'){
+// if(process.env.NODE_ENV === 'production'){
 
-    app.use(express.static('client/build'))
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname + "/client/build/index.html"))
-    })
-}
-
+//     app.use(express.static('client/build'))
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname + "/client/build/index.html"))
+//     })
+// }
 
 app.get('/', (req, res) => {
     res.send('EL SERVIDOR ESTÁ FUNCIONANDO!')
