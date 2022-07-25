132ef7d38c82e9298bfa250e7eb844ed23276e56
Kevin Ocampo <98836786+kevocode22@users.noreply.github.com>
2022-07-24 21:13:38 -0300

    Delete config directory

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGO_URI,{
-	useUnifiedTopology: true,
-	useNewUrlParser: true,
-})
-.then(()=>console.log('databased connect'))
-.catch(err => console.error(err))
