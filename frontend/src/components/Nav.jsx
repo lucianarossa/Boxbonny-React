0a636017384a56cede512ffa290ca1113b3458d4
VaRaOr <valen15k@gmail.com>
2022-07-26 20:30:05 -0300

    afinando detalles

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -16,7 +16,7 @@ import Badge from '@mui/material/Badge';
 
 export default function Nav() {
 
-  console.log("%cWelcome to the Console Park 🦖",'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+  // console.log("%cWelcome to the Console Park 🦖",'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
 
   const [anchorElUser, setAnchorElUser] = useState(null);
   const usuario = useSelector(store => store.usuariosReducer.user);
@@ -26,7 +26,7 @@ export default function Nav() {
   const products = useSelector(store => store.shoppingReducer.productos)
   async function desloguearse() {
     await dispatch(usuariosActions.desloguearse())
-      .then(navigate("/desloguearse", { replace: true }))
+      .then(navigate("/home", { replace: true }))
     //me lleva de nuevo al home al hacer sign out
   }
 
@@ -38,18 +38,20 @@ export default function Nav() {
     setAnchorElUser(null);
   };
 
-  const productosSum= products?.shopping
+  const productosSum = products?.shopping
   let contador = 0
-  productosSum?.map(c=>contador=contador + c.cantidad)
+  productosSum?.map(c => contador = contador + c.cantidad)
 
+  function admin() {
+    navigate("/administrador")
+  }
   return (
     <header className="sticky top-0  z-50  bg-[#F6F7EB]">
       <div className="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-1 md:flex md:items-center md:gap-12">
-            <img className="logo w-[12rem] h-[12rem]  " src="https://i.imgur.com/Xi3X0wB.png" alt="logo" />
+            <img className="logo w-[12rem] h-[12rem]" src="https://i.imgur.com/Xi3X0wB.png" alt="logo" />
           </div>
-
           <div className=" md:flex md:items-center md:gap-12">
             <nav className="hidden md:block" aria-labelledby="header-navigation">
               <h2 className="sr-only" id="header-navigation">Header navigation</h2>
@@ -63,7 +65,6 @@ export default function Nav() {
                     Inicio
                   </LinkRouter>
                 </li>
-
                 <li>
                   <LinkRouter
                     className="text-gray-500 transition hover:text-[#FF8E72]"
@@ -77,11 +78,9 @@ export default function Nav() {
                     className="relative flex items-center justify-center"
                     to="/cart"
                   >
-                       <Badge color="error" badgeContent={contador}>
-                       <ShoppingCartOutlinedIcon/>      
-                       </Badge>
-              
-
+                    <Badge color="error" badgeContent={contador}>
+                      <ShoppingCartOutlinedIcon />
+                    </Badge>
                   </LinkRouter>
                 </li>
               </ul>
@@ -90,11 +89,11 @@ export default function Nav() {
               {/* avatar usuario */}
               <Box sx={{ flexGrow: 0 }}>
                 <div>
-                  <Box  sx={{ display:"flex", p: 0 }}>
+                  <Box sx={{ display: "flex", p: 0 }}>
                     {usuario ? <Box sx={{ display: 'flex', flexDirection: 'column', WebkitJustifyContent: 'center', color: '#FF8E72', alignItems: 'center', }} >
                       <IconButton onClick={handleOpenUserMenu} >
-                      <Avatar alt="imagen del usuario" src={usuario?.imagen} sx={{ width: 30, height: 30 }} sm={{ width: 30, height: 30 }} />
-                      <p className="m-nombreAvatar pl-2">{usuario?.nombre}</p>
+                        <Avatar alt="imagen del usuario" src={usuario?.imagen} sx={{ width: 30, height: 30 }} sm={{ width: 30, height: 30 }} />
+                        <p className="m-nombreAvatar pl-2">{usuario?.nombre}</p>
                       </IconButton>
                     </Box>
                       :
@@ -127,18 +126,25 @@ export default function Nav() {
                   }}
                   open={Boolean(anchorElUser)}
                   onClose={handleCloseUserMenu}>
-                  {usuario ? (
-                    <Box>
-                      <MenuItem
-                        onClick={handleCloseUserMenu}>
-                        <Typography sx={{ padding: '2px', paddingLeft: '6px', paddingRight: '6px', color: 'rgb(2,0,3)' }} onClick={desloguearse}>Salir</Typography>
-
-                      </MenuItem>
-                    </Box>
-                  )
+                  {usuario ?
+                    usuario?.rol === "admin" ?
+                      <Box onClick={handleCloseUserMenu}>
+                        <MenuItem style={{ textAlign: "center", justifyContent: 'center' }}>
+                          <Typography sx={{ padding: '2px', paddingLeft: '6px', paddingRight: '6px', color: 'rgb(2,0,3)' }} style={{ fontFamily: "Poppins" }} onClick={desloguearse}>Salir</Typography>
+                        </MenuItem>
+                        <MenuItem>
+                          <Typography sx={{ padding: '2px', paddingLeft: '6px', paddingRight: '6px', color: 'rgb(2,0,3)' }} style={{ fontFamily: "Poppins" }} onClick={admin}>Cargar Experiencia</Typography>
+                        </MenuItem>
+                      </Box>
+                      :
+                      <Box onClick={handleCloseUserMenu}>
+                        <MenuItem style={{ textAlign: "center", justifyContent: 'center' }}>
+                          <Typography sx={{ padding: '2px', paddingLeft: '6px', paddingRight: '6px', color: 'rgb(2,0,3)' }} style={{ fontFamily: "Poppins" }} onClick={desloguearse}>Salir</Typography>
+                        </MenuItem>
+                      </Box>
                     :
                     <MenuItem
-                       onClick={handleCloseUserMenu}><Typography sx={{ padding: '2px', paddingLeft: '6px', paddingRight: '6px', color: 'rgb(2,0,3)' }}>No estás logueado</Typography></MenuItem>
+                      onClick={handleCloseUserMenu}><Typography sx={{ padding: '2px', paddingLeft: '6px', paddingRight: '6px', color: 'rgb(2,0,3)' }} style={{ fontFamily: "Poppins" }}>No estás logueado</Typography></MenuItem>
                   }
                 </Menu>
               </Box>
@@ -146,24 +152,24 @@ export default function Nav() {
           </div>
         </div>
         <div className="border-t border-gray-100 md:hidden lg:hidden">
-            <nav
-              className="flex items-center justify-center p-4 overflow-x-auto text-sm font-medium gap-6"
+          <nav
+            className="flex items-center justify-center p-4 overflow-x-auto text-sm font-medium gap-6"
+          >
+            <LinkRouter
+              className="text-gray-500 transition hover:text-[#FF8E72]"
+              to="/"
             >
-              <LinkRouter
-                    className="text-gray-500 transition hover:text-[#FF8E72]"
-                    to="/"
-                  >
-                    Inicio
-                  </LinkRouter>
-                  <LinkRouter
-                    className="text-gray-500 transition hover:text-[#FF8E72]"
-                    to="/packs"
-                  >
-                    Packs
-                  </LinkRouter>
+              Inicio
+            </LinkRouter>
+            <LinkRouter
+              className="text-gray-500 transition hover:text-[#FF8E72]"
+              to="/packs"
+            >
+              Packs
+            </LinkRouter>
 
-            </nav>
-          </div>
+          </nav>
+        </div>
       </div>
     </header>
   )
