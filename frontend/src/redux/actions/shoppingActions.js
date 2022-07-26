535252b8d5241a2ad87432f8e46b9738809dc51b
alexishermosa <alexishermosa@hotmail.com>
2022-07-26 13:35:01 -0300

    pull merge

--- frontend/src/redux/actions/shoppingActions.js
+++ frontend/src/redux/actions/shoppingActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const url = 'http://localhost:8000/'
-// const url=  'https://boxbonny-back.herokuapp.com/'
+// const url = 'http://localhost:8000/'
+const url=  'https://boxbonny-back.herokuapp.com/'
 
 const shoppingActions = {
 
